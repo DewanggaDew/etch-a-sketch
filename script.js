37b1dcdd93f6feaa9f25866869acd2a88b574ebb
DewanggaDew <dewangga.indera@gmail.com>
2024-02-18 20:51:16 +0800

    Major improvement on the grid and settings buttons and range slider. With a few improvements in styling

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const DEFAULT_COLOR = "#333333";
+const DEFAULT_MODE = "color";
 const DEFAULT_SIZE = 16;
 
 let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
 let currentSize = DEFAULT_SIZE;
 
 const grid = document.getElementById("grid");
-const input = document.getElementById("input");
+const input = document.getElementById("inputSlider");
+const inputValue = document.getElementById("inputValue");
+const btnClear = document.getElementById("clear");
+const colorPicker = document.getElementById("colorPicker");
+const btnEraser = document.getElementById("eraserMode");
+
+colorPicker.oninput = (e) => {
+  setCurrentColor(e.target.value);
+};
+input.onmousemove = (e) => {
+  updateInputValue(e.target.value);
+};
+input.onchange = (e) => changeSize(e.target.value);
+btnClear.onclick = () => reloadGrid();
 
 function createGrid(num) {
   grid.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
@@ -20,13 +35,36 @@ function createGrid(num) {
 }
 
 function draw(e) {
-  e.target.style.backgroundColor = "black";
+  e.target.style.backgroundColor = currentColor;
+}
+
+function setCurrentColor(newColor) {
+  currentColor = newColor;
+}
+
+function setCurrentSize(newSize) {
+  currentSize = newSize;
 }
 
 function changeSize(input) {
-  createGrid();
+  setCurrentSize(input);
+  updateInputValue(input);
+  reloadGrid();
+}
+
+function reloadGrid() {
+  clear();
+  createGrid(currentSize);
+}
+
+function updateInputValue(newValue) {
+  inputValue.innerHTML = `${newValue} x ${newValue}`;
 }
 
 function clear() {
   grid.innerHTML = "";
 }
+
+window.onload = () => {
+  createGrid(DEFAULT_SIZE);
+};
